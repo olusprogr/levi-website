10f4cb425c9609d6ac415bf00ed21464f3b0ac3a
olusprogr <olus.main333@gmail.com>
2024-05-25 17:10:17 +0200

    added navbar, footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    NavbarComponent,
+    NgbAlertModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
