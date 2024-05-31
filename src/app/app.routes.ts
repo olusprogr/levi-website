5d0afd374ade1a10a0cbb12a1e0d7d758779c308
olusprogr <olus.main333@gmail.com>
2024-05-31 23:12:38 +0200

    added carousel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+    },
+    {
+        path: 'home', component: StartComponent
+    },
+    {
+        path: '**', component: ErrorComponent
+    }
+];
---
+++ src/app/start/start.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-start',
+  standalone: true,
+  imports: [
+    NgbCarousel
+  ],
+  templateUrl: './start.component.html',
+  styleUrl: './start.component.css'
+})
+export class StartComponent {
+
+}
