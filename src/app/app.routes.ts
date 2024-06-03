6b51054227e11be196bfdc4d6379d686deda1d98
olusprogr <olus.main333@gmail.com>
2024-06-03 20:25:12 +0200

    added elementref and implement routings in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { StartComponent } from './start/start.component';
 import { ErrorComponent } from './error/error.component';
 import { ProductComponent } from './product/product.component';
+import { InformationComponent } from './information/information.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +14,12 @@ export const routes: Routes = [
     },
     {
         path: 'home/product/:productName', component: ProductComponent
+    },
+    {
+        path: 'home/information', component: InformationComponent
+    },
+    {
+        path: 'home/about-us', component: AboutUsComponent
     },
     {
         path: '**', component: ErrorComponent
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(
+    private router: Router,
+    private elementRef: ElementRef
+  ) {}
 
+  navigateTo(path: string): void {
+    if (path === 'home') {
+      this.router.navigate(['/home'])
+      return
+    } else if (path === 'products') {
+      const targetElement = this.elementRef.nativeElement.querySelector(path)
+      targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' }) // need to be fixed
+    }
+    this.router.navigate(["/home/" , path])
+  }
 }
