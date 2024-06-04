07f34e5c38ad13c399bb6751016e57fc047ec9f2
olusprogr <olus.main333@gmail.com>
2024-06-04 17:54:01 +0200

    tried to fix categories

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsService } from '../products.service';
 import { CommonModule } from '@angular/common';
@@ -26,14 +26,55 @@ import {MatButtonModule} from '@angular/material/button';
 })
 export class StartComponent implements OnInit {
   products: any = []
+  categoryProd: any = []
+  currentCategory: string = ''
 
   constructor(
     private productsService: ProductsService
   ) {
-    
+
   }
 
-  ngOnInit(): void {
+  updateProducts(): void {
     this.products = this.productsService.getProducts()
+  }
+  
+  ngOnInit(): void {
+    this.updateProducts()
+  }
+
+  private checkIfProductHasCategory(category: string, productList: any[]): boolean {
+    this.categoryProd = []
+    for (let product of productList) {
+      for (let prodCategory of product.categories) {
+        if (prodCategory === category) {
+          this.categoryProd.push(product)
+          return true
+        }
+      }
+    }
+    return false
+  }
+
+  toggleCetegory(categoryID: number): void {
+    const availableCategories: { [key: number]: string } = {
+      1: 'NEW❗',
+      2: 'Trended🔥',
+      3: 'Best Seller',
+      4: 'Discount',
+      5: 'Best price-performance-offer'
+    }
+    
+    if (availableCategories[categoryID] === undefined) {
+      return
+    } else {
+      this.currentCategory = availableCategories[categoryID]
+    }
+
+    if (this.checkIfProductHasCategory(this.currentCategory, this.products)) {
+      this.updateProducts()
+      this.products = this.categoryProd
+      console.log(this.products)
+    }
   }
 }
