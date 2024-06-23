c05afc9863ba722156b19226e92d3cd1e9050dd2
Nicole Chodura <nicoole@mbp-von-nicole.lan>
2024-06-23 18:01:02 -0400

    Revert "improved front-end"
    This reverts commit fdc0aa8e0268fd48fe541c1bacc3159e1b03b78b.

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -20,26 +20,18 @@ export class ProductsService implements OnInit{
   constructor(
   ) {
     this.products = (products as any).default as Product[]
-    this.shuffleArray(this.products.slice(0, 10))
+    
   }
-
-  private shuffleArray(array: any[]): void {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]
-        this.products = array
-    }
-}
 
   ngOnInit(): void {
 
   }
 
-  public getProducts(): Product[] {
+  getProducts(): Product[] {
     return this.products
   }
 
-  public searchForSpecificProduct(name: string): Product | undefined {
+  searchForSpecificProduct(name: string): Product | undefined {
     return this.products.find(product => product.name === name)
   }
 }
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsService } from '../products.service';
 import { CommonModule } from '@angular/common';
@@ -38,15 +38,8 @@ export class StartComponent implements OnInit {
   activeButton: number | null = null
 
   constructor(
-    private productsService: ProductsService,
-    {nativeElement}: ElementRef<HTMLElement> 
-  ) {
-    const support = 'loading' in HTMLImageElement.prototype
-    if (support) {
-      nativeElement.setAttribute('loading', 'lazy')
-    }
-  }
-
+    private productsService: ProductsService
+  ) {}
 
   updateProducts(): [] {
     this.categoryProd = []
