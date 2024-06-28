fc3a6a990f8b43996e26c75c003681bd3e58e607
Nicole Chodura <nicoole@mbp-von-nicole.lan>
2024-06-28 00:14:49 -0400

    fixe path to the api request

--- back-end/README.md
+++ back-end/README.md
@@ -4,3 +4,5 @@
 # savesphere-backend
 # savesphere-backend
 # savesphere-backend
+# savesphere-backend
+# savesphere-backend
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  baseURL: string = 'http://localhost:3000/api';
+  baseURL: string = 'https://savesphere-backend.onrender.com/api';
 
   constructor(
     private http: HttpClient,
