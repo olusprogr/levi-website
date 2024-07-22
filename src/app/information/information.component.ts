030bf537eeebdecd57ae02f394219204058c9236
olusprogr <olus.main333@gmail.com>
2024-07-22 18:46:20 -0400

    added statistics

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,10 +1,13 @@
 import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [],
+  imports: [
+    StatisticsComponent
+  ],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
